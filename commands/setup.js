a03d0c760892de30969923391bae254e2b285cbc
Jan Paul Posma <janpaul123@users.noreply.github.com>
2017-09-22 12:28:37 -0700

    Add comment about why we run all setup on all containers

--- commands/setup.js
+++ commands/setup.js
@@ -2,7 +2,9 @@
 const { log, runCommandGroup } = require('../lib.js');
 
 function setup(projects, options) {
-  log('Running setup...');
+  // TODO(JP): Do something more advanced for Circle CI caching (maybe with
+  // Circle CI 2.0) so we don't have to run all the setup on all containers.
+  log('Running setup (the same on all containers, so caching is straightforward)...');
   const env = options.ci ? 'ci' : 'osx';
 
   Object.keys(projects).forEach((projectName) =>
